e0a1677b86c4faf03720f65a5a1d8cd61be0546f
cyh2004 <chenying21@mails.tsinghua.edu.cn>
2024-01-28 14:04:16 +0800

    init

---
+++ setup_ubuntu.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt update && sudo apt install zsh --yes
+sudo apt install curl git python3-pip python vim net-tools
+sudo apt install ca-certificates --reinstall
+pip3 install wheel ranger-fm archey4
+./install_oh_my_zsh.sh
+sudo apt-get install python-pip
+git config --global user.name "cyh2004"
+git config --global user.email "chenying21@mails.tsinghua.edu.cn"
+touch ~/.gitignore_global
+echo ".DS_Store
+.vscode" >> ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
+
+sudo apt-get install fzf ||{
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+}
